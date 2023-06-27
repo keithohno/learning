5048a09fa29f01601f4d5b24e0a714624270339a
Keith Zhang <keithohno@gmail.com>
2023-06-27 09:11:54 -0400

    KZ-69: added plotting and expanded training function

--- pytorch/helpers.py
+++ pytorch/helpers.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+
+
 def eval_model(model, dataloader, loss_fn):
     loss = 0
     acc = 0
@@ -8,3 +11,11 @@ def eval_model(model, dataloader, loss_fn):
     loss /= len(dataloader)
     acc /= len(dataloader)
     return loss, acc
+
+
+def plot_model_criteria(train_list, test_list, epoch_list, path):
+    plt.figure()
+    plt.plot(epoch_list, train_list, label="training")
+    plt.plot(epoch_list, test_list, label="testing")
+    plt.legend()
+    plt.savefig(path)
--- pytorch/mnist/__init__.py
+++ pytorch/mnist/__init__.py
@@ -4,7 +4,7 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 from tqdm import tqdm
-from helpers import eval_model
+from helpers import eval_model, plot_model_criteria
 
 
 def get_dataloaders():
@@ -17,14 +17,21 @@ def get_dataloaders():
     return train_dataloader, test_dataloader
 
 
-def train(model, loss_fn, optimizer, train_dataloader, test_dataloader):
+def run_training_pipeline(model, model_name):
+    torch.manual_seed(23)
+    train_dataloader, test_dataloader = get_dataloaders()
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
     epoch_list = []
     train_loss_list = []
     train_acc_list = []
     test_loss_list = []
     test_acc_list = []
 
-    for epoch in tqdm(range(10)):
+    for epoch in tqdm(range(20)):
+        model.train()
         for x, y in train_dataloader:
             y_hat = model(x)
             loss = loss_fn(y_hat, y)
@@ -32,29 +39,28 @@ def train(model, loss_fn, optimizer, train_dataloader, test_dataloader):
             optimizer.step()
             optimizer.zero_grad()
 
-        if epoch % 2 == 0:
+        model.eval()
+        with torch.inference_mode():
             train_loss, train_acc = eval_model(model, train_dataloader, loss_fn)
-
             test_loss, test_acc = eval_model(model, test_dataloader, loss_fn)
 
             epoch_list.append(epoch)
-            train_loss_list.append(train_loss)
-            train_acc_list.append(train_acc)
-            test_loss_list.append(test_loss)
-            test_acc_list.append(test_acc)
+            train_loss_list.append(train_loss.detach())
+            train_acc_list.append(train_acc.detach())
+            test_loss_list.append(test_loss.detach())
+            test_acc_list.append(test_acc.detach())
 
-    return epoch_list, train_loss_list, train_acc_list, test_loss_list, test_acc_list
+    plot_model_criteria(
+        train_loss_list, test_loss_list, epoch_list, f"mnist/{model_name}_loss.png"
+    )
+    plot_model_criteria(
+        train_acc_list, test_acc_list, epoch_list, f"mnist/{model_name}_acc.png"
+    )
 
 
 def run():
-    torch.manual_seed(23)
-    train_dataloader, test_dataloader = get_dataloaders()
-
-    model = nn.Sequential(
+    model_linear = nn.Sequential(
         nn.Flatten(), nn.Linear(784, 128), nn.ReLU(), nn.Linear(128, 10)
     )
 
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-
-    train(model, loss_fn, optimizer, train_dataloader, test_dataloader)
+    run_training_pipeline(model_linear, "linear")
