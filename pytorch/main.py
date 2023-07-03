cf8eb566aacfcac7fc46baa2e2c0b28dd87747f0
Keith Zhang <35638530+keithohno@users.noreply.github.com>
2023-07-03 18:58:03 -0400

    KZ-82: move imports midfile

--- pytorch/main.py
+++ pytorch/main.py
@@ -1,10 +1,9 @@
-from classification_incircle import run as run0
-from classification_clusters import run as run1
-from classification_mnist import run as run2
-from autoencoder_mnist import run as run3
-
 print(
-    "\nExamples:\n" "  (0) classification_circle\n" "  (1) classification_clusters\n" "  (2) classification_mnist\n" "  (3) autoencoder_mnist\n"
+    "\nExamples:\n"
+    "  (0) classification_circle\n"
+    "  (1) classification_clusters\n"
+    "  (2) classification_mnist\n"
+    "  (3) autoencoder_mnist\n"
 )
 
 selection = input("Make a selection (default 3): ")
@@ -12,10 +11,21 @@ selection = input("Make a selection (default 3): ")
 print()
 
 if selection == "0":
-    run0()
+    import classification_incircle
+
+    classification_incircle.run()
+
 elif selection == "1":
-    run1()
+    import classification_clusters
+
+    classification_clusters.run()
+
 elif selection == "2":
-    run2()
+    import classification_mnist
+
+    classification_mnist.run()
+
 else:
-    run3()
+    import autoencoder_mnist
+
+    autoencoder_mnist.run()
