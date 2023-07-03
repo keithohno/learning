a262ff39176846ca2d321e0fc717a61a410bc7d0
Keith Zhang <35638530+keithohno@users.noreply.github.com>
2023-07-03 11:30:29 -0400

    KZ-76: added plotting with baseline loss

--- pytorch/autoencoder_mnist/__init__.py
+++ pytorch/autoencoder_mnist/__init__.py
@@ -3,6 +3,7 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
@@ -17,6 +18,8 @@ def get_dataloaders():
 
 
 def run():
+    torch.manual_seed(23)
+
     train_dataloader, test_dataloader = get_dataloaders()
 
     model = nn.Sequential(
@@ -45,7 +48,10 @@ def run():
     loss_fn = nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-2)
 
-    for epoch in tqdm(range(5)):
+    loss_list = []
+    baseline_loss_list = []
+
+    for _ in tqdm(range(50)):
         model.train()
         for x, _ in train_dataloader:
             x_hat = model(x)
@@ -60,4 +66,15 @@ def run():
             for x, _ in test_dataloader:
                 x_hat = model(x)
                 loss += loss_fn(x_hat, x)
-            print(f"Epoch: {epoch}, Loss: {loss / len(test_dataloader)}")
+            loss_list.append(loss / len(test_dataloader))
+
+            loss = 0
+            for x, _ in test_dataloader:
+                x_hat = model(x)
+                loss += loss_fn(x_hat, x.flip(0))
+            baseline_loss_list.append(loss / len(test_dataloader))
+
+    plt.plot(loss_list, label="loss")
+    plt.plot(baseline_loss_list, label="baseline")
+    plt.legend()
+    plt.savefig("autoencoder_mnist/loss.png")
