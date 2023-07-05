a3d91dc2274d75a1a0ae272087d960913d5d7156
Keith Zhang <35638530+keithohno@users.noreply.github.com>
2023-07-05 09:42:18 -0400

    KZ-85: run models on gpu

--- pytorch/autoencoder_mnist/__init__.py
+++ pytorch/autoencoder_mnist/__init__.py
@@ -8,7 +8,10 @@ from tqdm import tqdm
 
 from .models import ModelV1
 
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
+# trains/saves model and reports loss
 def run_training_pipeline(model, train_dataset, test_dataset, seed=23):
     torch.manual_seed(seed)
 
@@ -24,6 +27,7 @@ def run_training_pipeline(model, train_dataset, test_dataset, seed=23):
     for _ in tqdm(range(50)):
         model.train()
         for x, _ in train_dataloader:
+            x = x.to(DEVICE)
             x_hat = model(x)
             loss = loss_fn(x_hat, x)
             loss.backward()
@@ -34,8 +38,9 @@ def run_training_pipeline(model, train_dataset, test_dataset, seed=23):
         with torch.inference_mode():
             loss = 0
             for x, _ in test_dataloader:
+                x = x.to(DEVICE)
                 x_hat = model(x)
-                loss += loss_fn(x_hat, x)
+                loss += loss_fn(x_hat, x).item()
             loss_list.append(loss / len(test_dataloader))
 
     torch.save(model.state_dict(), f"autoencoder_mnist/data/{model.name()}.pt")
@@ -43,35 +48,39 @@ def run_training_pipeline(model, train_dataset, test_dataset, seed=23):
     return loss_list
 
 
+# plots loss charts
 def plot_loss_charts(models, loss_lists, colors):
     fig, ax = plt.subplots()
     for i in range(len(models)):
-        ax.plot(loss_lists[i], color=colors[i], label=models[i].name())
+        loss_list = [x.item() for x in loss_lists[i]]
+        ax.plot(loss_list, color=colors[i], label=models[i].name())
     ax.legend()
     ax.set_xlabel("Epoch")
     ax.set_ylabel("Loss")
     fig.savefig("autoencoder_mnist/results/loss.png")
 
 
+# samples dataset and plots original and reconstructed images
 def plot_sample_reconstruction(models, test_dataset, seed=23):
     SAMPLES = 8
     torch.manual_seed(seed)
     fig, axs = plt.subplots(SAMPLES, len(models) + 1, figsize=(16, 16))
     x, _ = next(iter(DataLoader(test_dataset, batch_size=SAMPLES, shuffle=True)))
+    x = x.to(DEVICE)
 
     for i, model in enumerate(models):
         model.eval()
-        x_hat = model(x).detach().numpy()
+        x_hat = model(x).detach()
         axs[0, i + 1].set_title(model.name())
         for j in range(SAMPLES):
             img = x_hat[j].squeeze()
-            axs[j, i + 1].imshow(img, cmap="gray")
+            axs[j, i + 1].imshow(img.cpu(), cmap="gray")
             axs[j, i + 1].axis("off")
 
     axs[0, 0].set_title("Original")
     for j in range(SAMPLES):
         img = x[j].squeeze()
-        axs[j, 0].imshow(img, cmap="gray")
+        axs[j, 0].imshow(img.cpu(), cmap="gray")
         axs[j, 0].axis("off")
 
     fig.savefig(f"autoencoder_mnist/results/samples.png")
@@ -92,7 +101,7 @@ def run():
     models = []
     latent_dims = [3, 5, 7, 10, 15]
     for latent_dim in latent_dims:
-        models.append(ModelV1(latent_dim))
+        models.append(ModelV1(latent_dim).to(DEVICE))
 
     loss_lists = []
     colors = [
