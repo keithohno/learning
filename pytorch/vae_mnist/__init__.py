7e7648634584d9acad5fcf2fa0fcce197aafae49
Keith Zhang <35638530+keithohno@users.noreply.github.com>
2023-07-10 22:54:01 -0400

    KZ-104: move computation to gpu

--- pytorch/vae_mnist/__init__.py
+++ pytorch/vae_mnist/__init__.py
@@ -1,7 +1,11 @@
-from .models import VAE
+import torch
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
+
+from .models import VAE
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def run():
@@ -17,8 +21,8 @@ def run():
     dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
     # model test run
-    model = VAE()
+    model = VAE().to(DEVICE)
     x, _ = next(iter(dataloader))
-    x_hat = model(x)
+    x_hat = model(x.to(DEVICE))
 
     assert x.shape == x_hat.shape
