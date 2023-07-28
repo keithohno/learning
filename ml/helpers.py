0e28165306f771d4e9635b51867aceda0cf8a2b1
Keith Zhang <keithohno@gmail.com>
2023-07-27 23:29:20 -0400

    KZ-141: use mps device if available

--- ml/helpers.py
+++ ml/helpers.py
@@ -9,7 +9,12 @@ def manual_seed(seed):
 
 
 def get_device():
-    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        return torch.device("mps")
+    else:
+        return torch.device("cpu")
 
 
 def get_dir(file):
